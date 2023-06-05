9ad6c04b24c325ea2945374c6e65e598edd66480
maryam <shabanzadeh1712@yahoo.com>
2023-06-05 20:01:05 +0200

    change dockerfile

--- backend/schemas/user.py
+++ backend/schemas/user.py
@@ -4,6 +4,6 @@
    #     "name":item["name"],
     #    "email":item["email"],
      #   "password":item["password"]
-    }
+   # }
 #def usersEntity(entity) -> list:
     #return [userEntity(item) for item in entity]
